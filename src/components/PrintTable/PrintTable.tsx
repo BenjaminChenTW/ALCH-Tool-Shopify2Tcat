f21009efa7a4db5fa09fdea53023b091c4ab51a9
BenjaminChenTW <s910190benjamin@gmail.com>
2024-07-03 21:32:02 +0800

    Add render

--- src/components/PrintTable/PrintTable.tsx
+++ src/components/PrintTable/PrintTable.tsx
@@ -1,14 +1,11 @@
-// const valueOverrideMapping
-
 import { Form, Table, TableProps, Typography } from "antd";
 import { useEffect, useState } from "react";
 
 import TcatOrder from "../../types/TcatOrder.interface";
 
-import type { ColumnType } from "antd/es/table/interface";
 type TcatOrderRecord = TcatOrder & { key: string };
 
-const columns = [
+const columns: TableProps<TcatOrderRecord>["columns"] = [
   {
     title: "收件人名稱",
     dataIndex: "收件人名稱",
@@ -39,20 +36,55 @@ const columns = [
     dataIndex: "代收金額或到付",
     key: "price",
     width: 100,
-    // render
+    render: (text) => (text === "" ? "-" : text),
   },
   {
     title: "件數",
     dataIndex: "件數",
     key: "amount",
     width: 80,
-    // render
   },
   {
     title: "品名",
     dataIndex: "品名(詳參數表)",
     key: "product_type",
-    // render
+    width: 150,
+    render: (text) => {
+      switch (text) {
+        case "1":
+          return "0001-一般食品";
+        case "2":
+          return "0002-名特產/甜點";
+        case "3":
+          return "0003-酒/油/醋/醬";
+        case "4":
+          return "0004-穀物蔬果";
+        case "5":
+          return "0005-水產/肉品";
+        case "6":
+          return "0006-3C";
+        case "7":
+          return "0007-家電";
+        case "8":
+          return "0008-服飾配件";
+        case "9":
+          return "0009-生活用品";
+        case "10":
+          return "0010-美容彩妝";
+        case "11":
+          return "0011-保健食品";
+        case "12":
+          return "0012-醫療相關用品";
+        case "13":
+          return "0013-寵物用品飼料";
+        case "14":
+          return "0014-印刷品";
+        case "15":
+          return "0015-其他";
+        default:
+          return "參數錯誤";
+      }
+    },
   },
   {
     title: "備註",
@@ -70,7 +102,19 @@ const columns = [
     title: "希望配達時間",
     dataIndex: "希望配達時間((詳參數表))",
     key: "deliver_time",
-    // render
+    width: 120,
+    render: (text) => {
+      switch (text) {
+        case "1":
+          return "13點前";
+        case "2":
+          return "14:00~18:00";
+        case "4":
+          return "不指定";
+        default:
+          return "參數錯誤";
+      }
+    },
   },
   {
     title: "出貨日期",
@@ -88,13 +132,39 @@ const columns = [
     title: "溫層",
     dataIndex: "溫層((詳參數表))",
     key: "temp",
-    // render
+    width: 100,
+    render: (text) => {
+      switch (text) {
+        case "1":
+          return "常溫";
+        case "2":
+          return "冷藏";
+        case "3":
+          return "冷凍";
+        default:
+          return "參數錯誤";
+      }
+    },
   },
   {
     title: "尺寸",
     dataIndex: "尺寸((詳參數表))",
     key: "size",
-    // render
+    width: 100,
+    render: (text) => {
+      switch (text) {
+        case "1":
+          return "60cm";
+        case "2":
+          return "90cm";
+        case "3":
+          return "120cm";
+        case "4":
+          return "150cm";
+        default:
+          return "參數錯誤";
+      }
+    },
   },
   {
     title: "寄件人姓名",
@@ -136,13 +206,15 @@ const columns = [
     title: "是否列印",
     dataIndex: "是否列印",
     key: "is_print",
-    // render
+    width: 100,
+    render: (text) => (text === "Y" ? "是" : "否"),
   },
   {
     title: "是否捐贈",
     dataIndex: "是否捐贈",
     key: "is_donate",
-    // render
+    width: 100,
+    render: (text) => (text === "Y" ? "是" : "否"),
   },
   {
     title: "統一編號",
@@ -218,23 +290,23 @@ export default function PrintTable(props: Readonly<{ data: TcatOrder[] }>) {
   //   }
   // };
 
-  const mergedColumns: TableProps<TcatOrderRecord>["columns"] = columns.map(
-    (col) => ({
-      ...col,
-      fixed: col.fixed as ColumnType<TcatOrderRecord>["fixed"],
-      // onCell: (record: TcatOrderRecord) => ({
-      //   title: col.title,
-      //   dataIndex: col.dataIndex,
-      //   record,
-      //   setIsEditing: () => {
-      //     setEditingKey(record.key);
-      //   },
-      //   handleSave: () => {
-      //     save(record.key);
-      //   },
-      // }),
-    })
-  );
+  // const mergedColumns: TableProps<TcatOrderRecord>["columns"] = columns.map(
+  //   (col) => ({
+  //     ...col,
+  //     fixed: col.fixed as ColumnType<TcatOrderRecord>["fixed"],
+  //     // onCell: (record: TcatOrderRecord) => ({
+  //     //   title: col.title,
+  //     //   dataIndex: col.dataIndex,
+  //     //   record,
+  //     //   setIsEditing: () => {
+  //     //     setEditingKey(record.key);
+  //     //   },
+  //     //   handleSave: () => {
+  //     //     save(record.key);
+  //     //   },
+  //     // }),
+  //   })
+  // );
 
   return (
     <>
@@ -248,7 +320,7 @@ export default function PrintTable(props: Readonly<{ data: TcatOrder[] }>) {
       </Title>
       <Form form={form} component={false}>
         <Table
-          columns={mergedColumns}
+          columns={columns}
           // components={{
           //   body: {
           //     cell: EditableCell,
@@ -256,6 +328,7 @@ export default function PrintTable(props: Readonly<{ data: TcatOrder[] }>) {
           // }}
           dataSource={data}
           scroll={{ x: 3500 }}
+          style={{ padding: "0 24px" }}
           pagination={false}
           bordered
         />
